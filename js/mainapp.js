be7aa1e6e2f7c5a46e7644b5f424d2902411e4d0
amassoud323 <166847845+amassoud323@users.noreply.github.com>
2025-09-16 14:42:48 +0100

    form regex and banner animation implemented

--- js/mainapp.js
+++ js/mainapp.js
@@ -1,3 +1,43 @@
+// Hamburger menu button functionality
 function menuButton() {
   document.getElementById("navbarLinks").classList.toggle("open");
 }
+
+// Contact form validation
+function validateForm() {
+  let a = document.forms["contactForm"]["fName"].value;
+  let b = document.forms["contactForm"]["sName"].value;
+  let c = document.forms["contactForm"]["email"].value;
+  let d = document.forms["contactForm"]["phone"].value;
+
+  // check that required fields are filled
+  if ((a == "") || (b == "") || (c = "")) {
+    alert("Please ensure your name and email address are entered.");
+    return false;
+  }
+
+  // check phone number is entered as an 11-digit number starting with 0 IF the field is filled
+  // second number check too?
+  if ((d != "") && (!/^0\d{10}$/.test(d))) {
+    alert("Please ensure your phone number is entered correctly.");
+    return false;
+  }
+}
+
+// Banner text animation
+function banner() {
+  const elem = document.getElementById("bannerAnimation");
+  let opacity = 0;
+  elem.style.opacity = opacity;
+  const id = setInterval(frame, 25);
+
+  function frame() {
+    if (opacity >= 1) {
+      clearInterval(id);
+    } else {
+      opacity += 0.02;
+      elem.style.opacity = opacity;
+    }
+  }
+}
+document.addEventListener("DOMContentLoaded", banner);
